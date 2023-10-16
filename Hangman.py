2797e3c390332c4d7ac384d2510a2398b18011f8
ps4star <hoenn4life@gmail.com>
2023-10-16 13:23:50 -0400

    cleaned up code; added comments; added difficulty selection screen

--- Hangman.py
+++ Hangman.py
@@ -1,14 +1,15 @@
 import random
-while True:
 
-#Categories you can select
+
+while True:
+  # Categories you can select
   animals = ['cat','dog','rat','lizard','cow','kangaroo','human','monkey','buffalo','goose','raven','crow','quial','ostrich']
   sports = ['basketball','football','soccer','hockey','rugby','tennis','golf','chess','fencing','volleyball','handball','lacrosse']
   random_words = ['wrathful','hippopotomonstrosesquippedaliophobia','supercalifragilisticexpialidocious','monday','rocks','moon','magic','xerox','zero','alpha','random','hangman','zetta','market','hello','rain']
   word_list = []
   GUESSED = False
   
-  #Greeting
+  # Greeting
   print("""
    _                                                       
   | |                                            
@@ -24,7 +25,9 @@ while True:
   print('S for sports')
   print('R for random')
   option = input("Enter the category: ")
-#Wordlist is chosen
+
+
+  # Wordlist is chosen
   if option.lower() == 'a':
     word_list = animals
   elif option.lower() =='s':
@@ -35,13 +38,32 @@ while True:
     print("Error! Please only input a, s, or r to choose the category!")
     break
   word = random.choice(word_list)
-#Main gameplay loop
-  def game_run():
+
+  print()
+  print('Choose a difficulty')
+  print('E for easy (8 tries)')
+  print('M for medium (6 tries)')
+  print('H for hard (4 tries)')
+  diff_sel = input("Enter the difficulty: ")
+  g_tries = -1
+
+  if diff_sel.lower() == 'e':
+    g_tries = 8
+  elif diff_sel.lower() == 'm':
+    g_tries = 6
+  elif diff_sel.lower() == 'h':
+    g_tries = 4
+
+  if g_tries == -1:
+    print("Error! Please only input e, m, or h to choose the difficulty!")
+    break
+
+  # Main gameplay loop
+  def game_run(num_tries):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     letters_guessed = []
     correct_letters = []
-    letters_correct = alphabet in word
-    tries = 6
+    tries = num_tries
     guessed = False
     display_hangman(6)
     print()
@@ -49,30 +71,32 @@ while True:
     print()
     display_lines()
 
-  #Guess loop
-    while guessed == False:
+    # Guess loop
+    while not guessed:
       print("\nLetters guessed thus far: ")
       for letter in letters_guessed:
         print(letter, end=" ")
       print(f"\nYou have this many tries left: {tries}")
-  #User inputs letter  
+      
+      # User inputs letter  
       guess = input("\nEnter a letter: ").lower()
-  #If user has a correct letter guess  
+      
+      # If user has a correct letter guess  
       if guess in word:
         display_hangman(tries)
         letters_guessed.append(guess)
         correct_letters.append(guess)
-        letters_correct = display_word(letters_guessed)
+        display_word(letters_guessed)
         display_lines()
         
-  #If user has an incorrect letter guess
+      # If user has an incorrect letter guess
       else:
         tries -= 1
         if tries == 0:
           break
         letters_guessed.append(guess)
         display_hangman(tries)
-        letters_correct = display_word(letters_guessed)
+        display_word(letters_guessed)
         display_lines()
         
       if set(word) == set(correct_letters):
@@ -80,13 +104,13 @@ while True:
         GUESSED = True
         break
         
-#Displays lines underneath letters for clarity
+  # Displays lines underneath letters for clarity
   def display_lines():  
     print("\r")
     for char in word:
       print("\u203E", end=" ")
 
-#Displays hangman ASCII art based on amount of incorrect answers
+  # Displays hangman ASCII art based on amount of incorrect answers
   def display_hangman(tries): 
     if tries == 6:
       print("\n  +---+  ")
@@ -149,26 +173,27 @@ while True:
     counter = 0
     correct_letters = 0
     for char in word:
-      if(char in letters_guessed):
+      if (char in letters_guessed):
         print(word[counter], end=" ")
         correct_letters += 1
       else:
         print(" ", end = " ")
       counter += 1
-  game_run()
+  game_run(g_tries)
   
-  #Win and Loose
-  if GUESSED == True:
+  # Win and Lose
+  if GUESSED:
     print(f"Congratulations, you win! The word was {word}!")
     print()
   else:
     print()
     print(f"Game over! The word was {word}!")
-  #Try Again
+
+  # Try Again
   try_again = input("Press 1 to try again, 0 to exit. ")
   try:
       try_again = int(try_again)
       if try_again == 0:
-          break # break out of this while loop
+          break
   except:
       print("Non number entered")
